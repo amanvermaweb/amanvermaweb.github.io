707ac395d715b9dc2abb8594bae193b4125aa373
Aman Verma <amanverma.codes@gmail.com>
2025-08-08 20:52:49 +0530

    Completed Home and About Section

--- README.md
+++ README.md
@@ -4,17 +4,21 @@ My personal portfolio showcasing my projects, skills, and contact info.
 Built with **HTML**, **Tailwind CSS**, and **JavaScript**.
 
 ## 🚀 Live Demo
+
 [Visit Website](https://amanverma2009.github.io)
 
 ## 📂 Features
+
 - Dark theme with smooth animations
 - Fully responsive design
 - Optimized for performance and SEO
 
 ## 🛠️ Tech Stack
+
 - HTML
 - Tailwind CSS
 - JavaScript
 
 ## 📜 License
+
 This project is licensed under the MIT License.
---
+++ src/main.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll("nav > div:nth-child(2) a");
+
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => {
+          link.classList.remove("nav-active");
+          if (link.getAttribute("href") === `#${entry.target.id}`) {
+            link.classList.add("nav-active");
+          }
+        });
+      }
+    });
+  },
+  { threshold: 0.6 }
+);
+sections.forEach((section) => observer.observe(section));
