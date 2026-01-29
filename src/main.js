2b8aaa9e36e713e64ea7ef60657685f5464649ee
Aman Verma <amanverma.codes@gmail.com>
2026-01-30 00:41:44 +0530

    removed contact me form

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 const sections = document.querySelectorAll("section[id]");
 const navLinks = document.querySelectorAll("nav > div:nth-child(2) a");
-const form = document.querySelector("form");
-const message = document.getElementById("form-message");
 
 let thresholdValue = window.innerWidth <= 768 ? 0.1 : 0.4;
 const observer = new IntersectionObserver(
@@ -21,13 +19,3 @@ const observer = new IntersectionObserver(
   
 );
 sections.forEach((section) => observer.observe(section));
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  const res = await fetch(form.action, { method: "POST", body: data });
-  if (res.ok) {
-    message.classList.remove("hidden");
-    form.reset();
-  }
-});
